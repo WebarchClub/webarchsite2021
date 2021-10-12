950363c2484145f89991046a8484634b8228254f
Antara Gupta <71868258+Antara25-10@users.noreply.github.com>
2021-10-12 19:56:26 +0530

    Updated clients

--- src/containers/Clients/Clients.js
+++ src/containers/Clients/Clients.js
@@ -1,33 +1,48 @@
 import React from "react";
-import { Container, Row, Col,Hidden } from 'react-grid-system';
-import './Clients.css';
+import { Container, Row, Col, Hidden } from 'react-grid-system';
+import './clients.css';
+import './logo.png'
 
 const Clients = () => {
 
   return (
-<Container fluid>
-
-  <Row align="stretch" style={{ height: '220px' }} debug>
-    <Col debug>1 of 3</Col>
-    <Col debug>2 of 3</Col>
-    <Col debug>3 of 3</Col>
-    <Hidden sm xs><Col debug>3 of 3</Col></Hidden>
+<Container>
+    <div class="text-center">
+  <Row align="center"  debug>
+    <Hidden sm xs><Col  style={{ height: '180px', width: '190px', margin:'1.2rem' }} debug ><img  style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '180px', width: '190px', margin:'1.2rem' }} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '180px', width: '190px', margin:'1.2rem' }} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
+    <Hidden xxl xl lg md xs><Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug ><img  style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug><img style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug><img style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
+    <Hidden xxl xl lg md sm><Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug ><img  style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug><img style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug><img style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
   </Row>
 
-  <Row align="stretch" style={{ height: '220px' }} debug>
-    <Col debug>1 of 3</Col>
-    <Col debug>2 of 3</Col>
-    <Col debug>3 of 3</Col>
-    <Hidden sm xs><Col debug>3 of 3</Col>
-    <Col debug>3 of 3</Col></Hidden>
+  <Row align="center" debug>
+  <Hidden sm xs><Col  style={{ height: '180px' , width: '190px', margin:'1.2rem' }} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col style={{ height: '180px'  , width: '190px', margin:'1.2rem'}}  debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '180px', width: '190px', margin:'1.2rem' }} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
+    <Hidden xxl xl lg md xs><Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug ><img  style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug><img style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug><img style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
+    <Hidden xxl xl lg md sm><Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug ><img  style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug><img style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug><img style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
   </Row>
-<Hidden xxl xl lg md>
-  <Row align="stretch" style={{ height: '220px' }} debug>
-    <Col debug>1 of 3</Col>
-    <Col debug>2 of 3</Col>
-    <Col debug>3 of 3</Col>
+  <Row align="center"debug>
+  <Hidden sm xs><Col  style={{ height: '180px', width: '190px' , margin:'1.2rem'}} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '180px', width: '190px', margin:'1.2rem' }} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '180px', width: '190px', margin:'1.2rem' }} debug><img style={{ height: '180px', width: '190px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
+    <Hidden xxl xl lg md xs><Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug ><img  style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug><img style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '120px', width: '120px', margin:'1.2rem' }} debug><img style={{ height: '120px', width: '120px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
+    <Hidden xxl xl lg md sm><Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug ><img  style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug><img style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col>
+    <Col  style={{ height: '80px', width: '80px', margin:'1rem' }} debug><img style={{ height: '80px', width: '80px' }} src="./logo.png" alt="logo"></img></Col></Hidden>
   </Row>
-  </Hidden>
+  </div>
 </Container>
   )}
 export default Clients;
