6d802fc59ffd36448c6cfd851f5e155d2ea0311e
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2021-10-13 20:48:17 +0530

    feat: About section added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,27 +1,30 @@
-import "./Home.css";
-import Testimonials from "./Testimonials/Testimonials";
-import Services from "./Services/Services";
-import Landing from "./Landing/landing";
-import Clients from "./Clients/Clients";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import ContactUs from "./Contact-Us/ContactUs";
+import './Home.css'
+import Testimonials from './Testimonials/Testimonials'
+import Services from './Services/Services'
+import Landing from './Landing/landing'
+import About from './About/about'
+import Clients from './Clients/Clients'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import ContactUs from './Contact-Us/ContactUs'
 
 const Home = () => {
   return (
-    <div className="Home">
+    <div className='Home'>
       <Landing />
-      <Header id="Services" title="Services" />
+      <Header id='About' title='About Us' />
+      <About />
+      <Header id='Services' title='Services' />
       <Services />
-      <Header id="Clients" title="Clients" alt />
+      <Header id='Clients' title='Clients' alt />
       <Clients alt />
-      <Header id="Testimonials" title="Testimonials" />
+      <Header id='Testimonials' title='Testimonials' />
       <Testimonials />
-      <Header id="Contact" title="Contact Us" alt />
+      <Header id='Contact' title='Contact Us' alt />
       <ContactUs alt />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
 export default Home
